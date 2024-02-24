e3691e3c9c7f1b6ca4030e9ab2b533c80eff8a18
Ilyas Bintang Prayogi <ilyas.prayogi@gmail.com>
2024-02-24 15:20:06 +0700

    Create Project

---
+++ Loliloan/Loliloan/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Loliloan
+//
+//  Created by Ilyas Bintang Prayogi on 24/02/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let nc = UINavigationController(rootViewController: LoanListVC())
+        window.rootViewController = nc
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+}
+
---
+++ Loliloan/Loliloan/Loan List/LoanListVM.swift
@@ -0,0 +1,8 @@
+//
+//  LoanListVM.swift
+//  Loliloan
+//
+//  Created by Ilyas Bintang Prayogi on 24/02/24.
+//
+
+import Foundation
